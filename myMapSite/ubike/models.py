b527d0ca78824a438117902cf0f3dcaffbbf9a9e
chinweihsu <dacerwei@gmail.com>
2016-01-28 16:11:38 +0800

    build model

--- myMapSite/ubike/models.py
+++ myMapSite/ubike/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class station(models.Model):
+	sno = models.CharField(max_length=100)
+	sna = models.CharField(max_length=100)
+	tot = models.IntegerField()
+	sbi = models.IntegerField()
+	sarea = models.CharField(max_length=100)
+	mday = models.DateTimeField()
+	lat = models.FloatField()
+	lng = models.FloatField()
+	ar = models.CharField(max_length=100)
+	sareaen = models.CharField(max_length=100)
+	aren = models.CharField(max_length=100)
+	bemp = models.IntegerField()
+	act = models.BooleanField(default=True)
+
+	def __str__(self):
+		return self.sna
