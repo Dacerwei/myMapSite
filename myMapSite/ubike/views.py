48419a8c8160d262c97103e3f7b78a87a7a64edd
chinweihsu <dacerwei@gmail.com>
2016-02-10 18:00:59 +0800

    add urllib and get data

--- myMapSite/ubike/views.py
+++ myMapSite/ubike/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from datetime import datetime
+import urllib.request
+from urllib.error import HTTPError
+from urllib.error import URLError
+import gzip
+import json
+
 
 def ubike(request):
-
+	url = urllib.request.Request("http://data.taipei/youbike")
+	url.add_header("Accept-encoding", "gzip")
+	try :
+		response = urllib.request.urlopen(url)
+	except urllib.HTTPError as e:
+		print(e)
+	except urllib.URLError as e:
+		print(e)
+	else:
+	    data =gzip.open(response,'rb').read().decode('utf8')
+	    dataInJson = json.loads(data)
 	return render(request,'ubike.html',{
 		'refresh_time': datetime.now(),
+		'mydata':dataInJson,
 		})
