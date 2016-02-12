3b737fdf9cd638f01c1084d5f27086dc731f07db
chinweihsu <dacerwei@gmail.com>
2016-02-12 19:18:24 +0800

    import data to database

--- myMapSite/ubike/models.py
+++ myMapSite/ubike/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 
 class station(models.Model):
-	sno = models.CharField(max_length=100)
-	sna = models.CharField(max_length=100)
-	tot = models.IntegerField()
-	sbi = models.IntegerField()
-	sarea = models.CharField(max_length=100)
-	mday = models.DateTimeField()
-	lat = models.FloatField()
-	lng = models.FloatField()
-	ar = models.CharField(max_length=100)
-	sareaen = models.CharField(max_length=100)
-	aren = models.CharField(max_length=100)
-	bemp = models.IntegerField()
-	act = models.BooleanField(default=True)
+	sno = models.IntegerField(max_length=100)	#站點代號
+	sna = models.CharField(max_length=100)	#場站名稱(中文)
+	tot = models.IntegerField()	#場站總停車格
+	sbi = models.IntegerField()	#場站目前車輛數量
+	sarea = models.CharField(max_length=100)	#場站區域(中文)
+	mday = models.DateTimeField()	#資料更新時間
+	lat = models.FloatField()	#緯度
+	lng = models.FloatField()	#經度
+	ar = models.CharField(max_length=100)	#地址(中文)
+	sareaen = models.CharField(max_length=100)	#場站區域(英文)
+	aren = models.CharField(max_length=100)	#地址(英文)
+	bemp = models.IntegerField()	#空位數量
+	act = models.BooleanField(default=True)	#全站禁用狀態
 
 	def __str__(self):
 		return self.sna
+
--- myMapSite/ubike/views.py
+++ myMapSite/ubike/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from datetime import datetime
+from ubike.models import station
 import urllib.request
 from urllib.error import HTTPError
 from urllib.error import URLError
@@ -17,9 +18,47 @@ def ubike(request):
 	except urllib.URLError as e:
 		print(e)
 	else:
-	    data =gzip.open(response,'r').read().decode("utf8")
-	    dataInJson = json.loads(data)
+	    data =json.loads(gzip.open(response,'r').read().decode("utf8"))
+	    if not station.objects.all():
+	    	station_create(data)
+	    else:
+	    	station_update(data)
 	return render(request,'ubike.html',{
 		'refresh_time': datetime.now(),
-		'station_data': dataInJson,
+		'station_data': data,
 		})
+
+def station_create(new_data):
+	all_stations = new_data["retVal"]
+	for s,v in all_stations.items():
+		station.objects.create(
+			id = v["sno"],
+			sno = v["sno"],
+			sna = v["sna"],
+			tot = v["tot"],
+			sbi = int(v["sbi"]),
+			sarea = v["sarea"],
+			mday = datetime.strptime(v["mday"],"%Y%m%d%H%M%S"),
+			lat = float(v["lat"]),
+			lng = float(v["lng"]),
+			ar = v["ar"],
+			sareaen = v["sareaen"],
+			aren = v["aren"],
+			bemp = int(v["bemp"]),
+			act = v["act"]
+			)
+
+def station_update(new_data):
+	all_stations = new_data["retVal"]
+	for s,v in all_stations.items():
+		theStation = station.objects.filter(id = s)
+		theStation.update(
+			tot = v["tot"],
+			sbi = int(v["sbi"]),
+			mday = datetime.strptime(v["mday"],"%Y%m%d%H%M%S"),
+			bemp = int(v["bemp"]),
+			act = v["act"]
+			)
+
+
+
